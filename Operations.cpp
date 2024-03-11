88f59011429f6df3c638a4f84e087b769dc1c5fc
DupuySalleZoe <f.zoeds@gmail.com>
2024-03-11 14:21:50 +0100

    ajout fonction mult div

--- Operations.cpp
+++ Operations.cpp
@@ -17,3 +17,19 @@ int soustraction(int a, int b)
 	
 	return resultat;
 }
+int div(int a, int b)
+{
+	int resultat;
+	
+	resultat = a / b;
+	
+	return resultat;
+}
+int mult(int a, int b)
+{
+	int resultat;
+	
+	resultat = a * b;
+	
+	return resultat;
+}
