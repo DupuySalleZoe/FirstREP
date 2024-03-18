77ecad32636e55172574a78fc2fc6a16a0fcc613
DupuySalleZoe <162138109+DupuySalleZoe@users.noreply.github.com>
2024-03-18 14:18:06 +0100

    Update Operations.h
    enfin compris la fonction de h.

--- Operations.h
+++ Operations.h
@@ -1,26 +1,5 @@
 int mult(int a, int b);
 
-{
-	int resultat;
-	
-	resultat = a * b;
-	
-	return resultat;
-}
-
 int div(int a, int b);
-{
-	int resultat;
-	
-	resultat = a / b;
-	
-	return resultat;
-}
+
 int add(int a, int b);
-{
-	int resultat;
-	
-	resultat = a + b;
-	
-	return resultat;
-}
