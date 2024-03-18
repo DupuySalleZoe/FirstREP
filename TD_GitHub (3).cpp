b5bc436366f7167fcacc403212ed5e8a0a32c598
DupuySalleZoe <f.zoeds@gmail.com>
2024-03-18 15:24:47 +0100

    Update TD_GitHub (3).cpp

--- TD_GitHub (3).cpp
+++ TD_GitHub (3).cpp
@@ -11,7 +11,50 @@
 
 //Fonctions réalisées par l'étudiant 2 :
 
+bool estPair(int nb) {
+if (nb%2==0 || nb%==0) {
+    return vrai;
+    }
+    else {
+    return faux;
+    }}
 
+int sommeNombre(int nb) {
+    int somme = 0;
+    for (int i = nb; i >= 1; i--) {
+        somme += i;
+    }
+    return somme;
+}
+
+
+double salaireNet(double salaire); {
+
+double salaireNet = salaire * (1 - 0.23);
+    
+    
+    double prime = salaireNet * 0.12;
+    
+    
+    salaireNet += prime;
+    
+    return salaireNet;
+	
+	}
+
+int plusPetit(int a, int b, int c); {
+    int plusPetit = a; 
+   
+    if (b < plusPetit) {
+        plusPetit = b;
+    }
+    
+    if (c < plusPetit) {
+        plusPetit = c;
+    }
+    
+    return plusPetit;
+}
 
 
 
